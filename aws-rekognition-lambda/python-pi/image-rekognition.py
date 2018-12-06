086ddf5bc92eab84dc20035a88a77b76296246d0
Zankhna Randeri <zankhna.randeri@gmail.com>
2018-12-06 03:40:52 -0800

    Identify family member in captured image from Pi

--- aws-rekognition-lambda/python-pi/image-rekognition.py
+++ aws-rekognition-lambda/python-pi/image-rekognition.py
@@ -1,5 +1,39 @@
 import boto3
 
+def identify_family(source_file):
+    conn = boto3.client('s3')
+    rekognition_client = boto3.client('rekognition')
+    family_list = conn.list_objects(Bucket='smartcamerabucket', Prefix='family/', Delimiter='/')['Contents']
+    family_list.pop(0)
+    for key in family_list:
+        print(key['Key'])
+        file_name = key['Key']
+
+        # Local File
+        imageSource = open(source_file, 'rb')
+        # imageTarget = open(targetFile, 'rb')
+
+        compareface_response = rekognition_client.compare_faces(SimilarityThreshold=70, SourceImage={
+            'S3Object': {'Bucket': "smartcamerabucket", 'Name': file_name}},
+                                                                TargetImage={'Bytes': imageSource.read()})
+
+        # compareface_response = rekognition_client.compare_faces(
+        #     SimilarityThreshold=95,
+        #     SourceImage={'S3Object': {'Bucket': "smartcamerabucket", 'Name': file_name}},
+        #     TargetImage={'S3Object': {'Bucket': "smartcamerabucket", 'Name': source_file}})
+
+        for faceMatch in compareface_response['FaceMatches']:
+            confidence = faceMatch['Face']['Confidence']
+            if confidence > 95:
+                print("Family memeber found")
+                name_list = file_name.split("/")
+                name = name_list[1].split(".")
+                print("Family name : " + name[0])
+                break
+
+    # for key in bucket.get_all_keys(prefix='family/', delimiter='/'):
+    #     print(key.name)
+
 
 if __name__ == "__main__":
     fileName = 'target3.JPG'
@@ -27,4 +61,5 @@ if __name__ == "__main__":
     for label in expectedLabels:
         if label in detectedLabelNames:
             print('Expected Human label is present: ' + label)
+            identify_family(fileName)
             break
