688d6e2af9ca031ec4d4ddd681299192700742d9
amruthasingh <amruthasingh.b@gmail.com>
2018-12-09 13:32:24 -0800

    deploy cicd pipeline1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-export AWS_ACCESS_KEY_ID=AKIAIN52MG6YJQBYVLGA
-export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-east-1
-scp -i /var/lib/jenkins/.ssh/MyKeyPair.pem index.html root@52.90.14.25:/var/www/html/
+scp -i /var/lib/jenkins/.ssh/MyKeyPair.pem scripts/index.html root@52.90.14.25:/var/www/html/
